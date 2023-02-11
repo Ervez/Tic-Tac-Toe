49b94e121ed3fa74128730f1799a7f872393fee0
Ervez <fb.blaszkiewicz@gmail.com>
2023-02-11 20:10:19 +0100

    modal and winner logic

--- main.js
+++ main.js
@@ -20,8 +20,39 @@ $(function(){
     });
 
     function pick(event){
+        const {row, column} = event.target.dataset;
         const turn = round % 2 === 0 ? P2 : P1;
+        if(board[row][column] !== '') return;
         event.target.classList.add(turn);
+        board[row][column] = turn;
         round++;
+
+        console.log(checkEnd());
+        if(checkEnd() !== undefined){
+            $('#myModal').modal('show');
+            $('#myModalTitle').text(checkEnd() + ' is a WINNER!');
+            $('.box').attr('class', 'box fa');
+        }
+    }
+
+    function checkEnd(){
+        const result = board.reduce((total, row) => total.concat(row));
+        let winner;
+        let moves = {
+            'fa-times': [],
+            'fa-circle-o': []
+        };
+        
+        result.forEach((field, index) => moves[field] ? moves[field].push(index) : null);
+        combinations.forEach(combination => {
+            if(combination.every(index => moves[P1].indexOf(index) > -1)){
+                winner = 'Player 1';
+            }else if (combination.every(index => moves[P2].indexOf(index) > -1))
+            {
+                winner = 'Player 2';
+            }
+        })
+
+        return winner;
     }
 })
